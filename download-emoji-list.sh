ec0f996f83639b170ddd21342ee3b0c4a6c8fdc6
Ondřej Žára <ondrej.zara@gmail.com>
2020-04-08 22:21:16 +0200

    no squares

--- download-emoji-list.sh
+++ download-emoji-list.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-curl https://unicode.org/Public/emoji/12.0/emoji-sequences.txt | grep Basic_Emoji | grep -v "^#" | grep -v FE0F | cut -d " " -f 1 > emoji-list.txt
+curl https://unicode.org/Public/emoji/12.0/emoji-sequences.txt \
+	| grep Basic_Emoji \
+	| grep -v "^#" \
+	| grep -v FE0F \
+	| grep -v square \
+	| cut -d " " -f 1 \
+	> emoji-list.txt
 # cat test-data.txt | grep Basic_Emoji | grep -v "^#" | grep -v FE0F | cut -d " " -f 1 > emoji-list.txt
